481f5cdb2d2edc722b7427576cf674f77973c048
Betelhem Yehuala <167322606+Bete3@users.noreply.github.com>
2025-07-26 15:59:49 +0300

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import JobListingDashboard from "@/components/JobListingDashboard"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <JobListingDashboard />
+    </main>
+  )
+}
